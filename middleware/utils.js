7b029f73387a70683126c34040c9f3a0099cc241
yashhhh10 <yarora_be22@thapar.edu>
2025-09-24 04:43:57 +0530

    add user register with JWT generation

--- middleware/utils.js
+++ middleware/utils.js
@@ -1,1 +1,9 @@
-const require 
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id,role) => {
+    return jwt.sign({id,role},process.env.JWT_SECRET, {
+        expiresIn:'7d'
+    });
+};
+
+module.exports={generateToken};
---
+++ routes/signup.js
@@ -0,0 +1,27 @@
+const express= require('express');
+const user= require('../models/users');
+const {generateToken}= require('../middleware/utils');
+
+const router= express.Router();
+
+router.post("/register",async(req,res)=>{
+    try{
+        const{ name, email, password,role } = req.body;
+        const UserExists= await user.findOne({email});
+        if(UserExists) return res.status(400).json({message:"message user exists"});
+        
+        const user =await user.create({name , email ,password,role});
+
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+            role:user.role,
+            token:generateToken(user._id,user.role)
+        });
+    } catch(error){
+        res.status(500).json({message:"server error"});
+    }
+});
+module.exports= router;
+
