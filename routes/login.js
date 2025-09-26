37733ba562e853521a73d84ff524953535c6179f
yashhhh10 <yarora_be22@thapar.edu>
2025-09-26 17:20:49 +0530

    added team route

---
+++ routes/login.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const user=require('../models/users');
+const {generateToken}=require('../middleware/utils');
+
+const router=express.Router();
+
+router.post('/login',async(requestAnimationFrame,res)=>{
+        const {email,password}=requestAnimationFrame.body;
+        const userFound=await user.findOne({email});
+        if(userFound &&(await userFound.comparePassword(passwword))){
+            res.json({
+                _id:userFound._id,
+                name:userFound.name,
+                email:userFound.email,
+                role:userFound.role,
+                token:generateToken(userFound._id,userFound.role)
+            });
+        } else{
+            res.status(401).json({message:"invalid email or password"});
+        }
+    });
+    
+module.exports=router;
+
+
+    
---
+++ routes/team.js
@@ -0,0 +1,30 @@
+const express= require('express');
+const Team= require('../models/team');
+const League= require('../models/league');
+
+const router= express.Router();
+
+router.post('/add',async(req,res)=>{
+    const {name, league, shortname, stadium, coach, founded}= req.body;
+    try{
+        const userLeague= await League.findById(league);
+        if(!userLeague)return res.status(400).json({message:"league not found"});
+
+        const newTeam= new Team({
+            name, league, shortname, stadium, coach, founded
+        });
+        const savedTeam= await newTeam.save();
+        res.status(201).json(savedTeam);
+    } catch (error) {
+        res.status(500).json({message:"server error"});
+    }
+});
+router.get('/',async(req,res)=>{
+    try{
+        const teams = await Team.find().populate('league','leagueName country seasons');
+        res.json(teams);
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message:"server error"});
+    }
+})   
